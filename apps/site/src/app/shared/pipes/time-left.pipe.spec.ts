07655394bd35fee26f4d400e3fbac3f9c70e47e4
Daniel Marin <danielarturomt@gmail.com>
2020-11-17 18:47:00 -0300

    feat: add time left pipe

---
+++ apps/site/src/app/shared/pipes/time-left.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TimeLeftPipe } from './time-left.pipe';
+
+describe('TimeLeftPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TimeLeftPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- apps/site/src/app/shared/shared.module.ts
+++ apps/site/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SafeUrlPipe } from './pipes/safe-url.pipe';
-import { FooterComponent } from './components/footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { FooterComponent } from './components/footer/footer.component';
+import { SafeUrlPipe } from './pipes/safe-url.pipe';
+import { TimeLeftPipe } from './pipes/time-left.pipe';
+
 @NgModule({
-  declarations: [SafeUrlPipe, FooterComponent],
+  declarations: [SafeUrlPipe, FooterComponent, TimeLeftPipe],
   imports: [CommonModule, FontAwesomeModule],
-  exports: [SafeUrlPipe, FooterComponent],
+  exports: [SafeUrlPipe, FooterComponent, TimeLeftPipe],
 })
 export class SharedModule {}
