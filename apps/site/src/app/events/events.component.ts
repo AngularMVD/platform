ba1d4c2b9be6ca955b207248f03f5185e1e6cab5
Daniel Marin <danielarturomt@gmail.com>
2020-11-17 20:23:08 -0300

    feat: finish up events page

--- apps/site/src/app/events/events.component.ts
+++ apps/site/src/app/events/events.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { ScullyRoutesService } from '@scullyio/ng-lib';
+import { map } from 'rxjs/operators';
+
+import { Event } from '../core/classes/event.class';
 
 @Component({
   selector: 'site-events',
   templateUrl: './events.component.html',
-  styles: [],
+  styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent {
+  events$ = this.scully.available$.pipe(
+    map((availableRoutes) =>
+      availableRoutes
+        .filter((availableRoute) => {
+          const urlAsArray = availableRoute.route
+            .split('/')
+            .filter((segment) => !!segment);
+          return urlAsArray.length === 2 && urlAsArray[0] === 'events';
+        })
+        .map((availableRoute) => Event.fromMarkdown(availableRoute))
+    )
+  );
+
   constructor(private scully: ScullyRoutesService) {}
 }
--- apps/site/src/app/events/events.module.ts
+++ apps/site/src/app/events/events.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { EventsComponent } from './events.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [EventsComponent],
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild([{ path: '', component: EventsComponent }]),
   ],
 })
