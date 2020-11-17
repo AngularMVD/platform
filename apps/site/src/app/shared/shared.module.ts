ec0bdf4e17f1e0d5a02c81dfc7be2561ee985d40
Daniel Marin <danielarturomt@gmail.com>
2020-11-17 16:20:50 -0300

    feat: create a new footer component

--- apps/site/src/app/shared/shared.module.ts
+++ apps/site/src/app/shared/shared.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafeUrlPipe } from './pipes/safe-url.pipe';
-
-
+import { FooterComponent } from './components/footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [SafeUrlPipe],
-  imports: [
-    CommonModule
-  ],
-  exports: [SafeUrlPipe]
+  declarations: [SafeUrlPipe, FooterComponent],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [SafeUrlPipe, FooterComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
