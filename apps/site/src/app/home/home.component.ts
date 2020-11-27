e7845dd1620b77aeb5b1417e280b1db44edaa763
Daniel Marin <danielarturomt@gmail.com>
2020-11-27 19:57:44 -0300

    fix: redirect user to events when landing in home

--- apps/site/src/app/home/home.component.ts
+++ apps/site/src/app/home/home.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'site-home',
   templateUrl: './home.component.html',
   styles: [],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.navigate(['events']);
+  }
+}
